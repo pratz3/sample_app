c85e533f20f6590bc42b1434f3a485902a52323d
Prateek Shinde <pshinde@live.com>
2012-12-03 21:02:46 +0530

    Static pages Done

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,9 +4,40 @@ describe "StaticPages" do
   
   describe "Home Page" do
     
-    it "should have Sample App" do
+    it "should have h1 Home" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1', :text =>'Home')
+    end
+    
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => " Home")
     end
   end
+  
+  describe "About Page" do
+    
+    it "should have h1 About Us" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => "About Us")
+    end
+    
+    it "should have title About Page" do
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => " About Us")
+    end
+  end
+  
+  describe "Help Page" do
+    
+    it "should have h1 Help" do
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text => "Help")
+    end
+    
+    it "should have title Help" do
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => " Help")
+    end
+  end  
 end
